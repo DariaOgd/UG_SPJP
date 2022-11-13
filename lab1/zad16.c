544f6763ffe8538da1edd7df3a928fee0ca5835d
DariaOgd <daria.ogd@gmail.com>
2022-11-13 11:51:51 +0100

    zad16

--- lab1/zad16.c
+++ lab1/zad16.c
@@ -1,38 +1,54 @@
 #include <stdio.h>
 
-int main(){
+int main() {
+
     int a;
     int counter = 0;
-    int liczba = 5;
+    int res = 15;
     printf("Odgadnij liczbe od 1-5 \n");
     scanf("%d", &a);
-    
+    res-=a;
    
-   if(a!=liczba && counter !=4){
-    counter++;
-    liczba=liczba-a;
-    printf("Nie zgadłes, spróbuj jeszcze raz");
-    scanf("%d", &a);
-
-    if(a!=liczba && counter !=4){
-    counter++;
-    liczba=liczba-a;
-    printf("Nie zgadłes, spróbuj jeszcze raz");
-    printf(counter);
-    scanf("%d", &a);
-
-    if(a!=liczba && counter !=4){
-    counter++;
-    liczba=liczba-a;
-    printf("Nie zgadłes, spróbuj jeszcze raz");
-    scanf("%d", &a);
-    if(counter==4){
-        printf("Liczba to\n");
+    if(a>5){
+        printf("Zła liczba, za duzo");
+    }else {
+     if(counter<=3){
+        res-=a;
+        counter++;
+       printf("Nie zgadłeś, spróbuj jeszcze raz ");
+        scanf("%d", &a);
+        //printf("%d res to  \n",res);
+        
+        if(counter<=3){
+            res-=a;
+            counter++;
+            
+            //res-=a;
+          //printf("%d res to  \n",res);
+          printf("Nie zgadłeś, spróbuj jeszcze raz ");
+            scanf("%d ", &a);
+            if(1>0){
+            counter++;
+            res-=a;
+          
+            //printf("xc ");
+            //scanf("%d ", &a);
+        }
+        }
+        if(counter==3){
+            counter++;
+            res-=a;
+            //printf("%d  \n", res);
+            printf("Nie zgdałeś  poprawna liczba to %d", res);
+            
+            
+        }
+        
         
     }
-   }
-   } 
-   }
-    
-   }
+    }
+
+
+  return 0;
+}
 
